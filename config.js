84da297d6a65e2170bf058cdc98e2bcebf8f8700
nana243 <106389343+nana243@users.noreply.github.com>
2022-06-07 11:56:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6288233832771'],
+  ['6288233832771'],
+  ['6281348568830', 'Queen Nana', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = '© Nana Bot'
+global.author = 'By Mursid'
 
 global.multiplier = 69 // The higher, The harder levelup
 
